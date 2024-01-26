b21f90b222faa4d5334069119accf9f0f83e8f37
sun.kist <sun.kist@kakaocorp.com>
2024-01-26 20:22:34 +0900

    feat: daisy ui 도입

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 const envImageUnoptimize = isProduction ? false : true;
 
 const nextConfig = {
@@ -7,11 +7,8 @@ const nextConfig = {
   images: {
     unoptimized: envImageUnoptimize,
   },
-  basePath:
-    isProduction !== 'development' ? undefined : '/jparkk0517.github.io',
-  assetPrefix: isProduction
-    ? 'https://github.com/jparkk0517/jparkk0517.github.io/tree/gh-pages'
-    : '',
+  basePath: !isProduction ? undefined : '/jparkk0517.github.io',
+  assetPrefix: isProduction ? '/' : undefined,
 };
 
 export default nextConfig;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,35 @@
+import { useRef } from 'react';
+
 const Home = () => {
-  return <>home!!</>;
+  const modalRef = useRef<HTMLDialogElement>(null);
+  return (
+    <>
+      <button className='btn btn-primary'>Hello daisyUI!</button>
+      lwejfoiawejfoaw!
+      <button
+        className='btn'
+        onClick={() => {
+          if (!modalRef.current) return;
+          modalRef.current.showModal();
+        }}>
+        open modal
+      </button>
+      <dialog id='my_modal_1' className='modal' ref={modalRef}>
+        <div className='modal-box'>
+          <h3 className='font-bold text-lg'>Hello!</h3>
+          <p className='py-4'>
+            Press ESC key or click the button below to close
+          </p>
+          <div className='modal-action'>
+            <form method='dialog'>
+              {/* if there is a button in form, it will close the modal */}
+              <button className='btn'>Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
 };
 
 export default Home;
