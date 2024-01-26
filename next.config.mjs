e5f4a807d2d9ab5d5a51c411cbb0021abff2e556
sun.kist <sun.kist@kakaocorp.com>
2024-01-26 19:55:49 +0900

    feat: 기본설정(prod, dev 모드에 따른 거동 상이)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProduction = process.env.NODE_ENV !== 'production';
+const envImageUnoptimize = isProduction ? false : true;
+
+const nextConfig = {
+  output: isProduction ? undefined : 'export',
+  images: {
+    unoptimized: envImageUnoptimize,
+  },
+  basePath:
+    isProduction !== 'development' ? undefined : '/jparkk0517.github.io',
+  assetPrefix: isProduction
+    ? 'https://github.com/jparkk0517/jparkk0517.github.io/tree/gh-pages'
+    : '',
+};
 
 export default nextConfig;
