26bc97f4f29bddbf57190f6c51fc85f78254cc50
sun.kist <sun.kist@kakaocorp.com>
2024-01-29 19:34:14 +0900

    Updates

--- _next/static/yVD_4AFhd8UqW2TdTegKY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9a890acb1e81c3fc.js"],"/home":["static/chunks/pages/home-c16621017cf87160.js"],sortedPages:["/_app","/_error","/home"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
